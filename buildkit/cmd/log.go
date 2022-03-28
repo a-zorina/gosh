7514d1161ff877f2e9d0101b4abff6943599ed4e
Sergei Blinov <blinovsv@gmail.com>
2022-03-29 00:57:04 +0300

    add signed example + refactoring

--- buildkit/cmd/log.go
+++ buildkit/cmd/log.go
@@ -1,23 +1,29 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
+	"strconv"
 )
 
 var WebLogEnabled bool = false
 
 func initWebLog() {
-	v := strings.ToLower(strings.TrimSpace(os.Getenv("WEBLOG")))
-	if v != "" && v != "0" && v != "no" && v != "false" {
+	v, err := strconv.ParseBool(os.Getenv("WEBLOG"))
+	if v && err == nil {
 		WebLogEnabled = true
 	}
 }
 
-func p(msg string) string {
+func dump(v interface{}) string {
+	b, _ := json.Marshal(v)
+	return string(b)
+}
+
+func sendWebLog(msg string) string {
 	if WebLogEnabled {
 		http.Get("http://localhost:8888/?" + url.Values{"log": {msg}}.Encode())
 	}
@@ -25,5 +31,5 @@ func p(msg string) string {
 }
 
 func logf(format string, a ...interface{}) string {
-	return p(fmt.Sprintf(format, a...))
+	return sendWebLog(fmt.Sprintf(format, a...))
 }

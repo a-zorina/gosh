d84c93375c9d5aca09aa8e133e4907d6a3ae5c83
Vasily Selivanov <v.selivanov@gmail.com>
2022-03-28 12:50:05 +0300

    updated `deploy_gosh.sh`: added loading of the snapshot contract code

--- solidity/scripts/deploy_gosh.sh
+++ solidity/scripts/deploy_gosh.sh
@@ -15,17 +15,25 @@ echo "[deploy $fn]"
 ./deploy_contract.sh $fn 100000000000 || exit 1
 GOSH_ADDR=$(cat $fn.addr)
 
+echo load `repo`-contract
 REPO_CODE=$($TVM_LINKER decode --tvc ../repository.tvc | sed -n '/code:/ s/ code: // p')
 REPO_DATA=$($TVM_LINKER decode --tvc ../repository.tvc | sed -n '/data:/ s/ data: // p')
 $TONOS_CLI -u $NETWORK call $GOSH_ADDR setRepository "{\"code\":\"$REPO_CODE\",\"data\":\"$REPO_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
 
+echo load `commit`-contract
 COMMIT_CODE=$($TVM_LINKER decode --tvc ../commit.tvc | sed -n '/code:/ s/ code: // p')
 COMMIT_DATA=$($TVM_LINKER decode --tvc ../commit.tvc | sed -n '/data:/ s/ data: // p')
 $TONOS_CLI -u $NETWORK call $GOSH_ADDR setCommit "{\"code\":\"$COMMIT_CODE\",\"data\":\"$COMMIT_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
 
+echo load `blob`-contract
 BLOB_CODE=$($TVM_LINKER decode --tvc ../blob.tvc | sed -n '/code:/ s/ code: // p')
 BLOB_DATA=$($TVM_LINKER decode --tvc ../blob.tvc | sed -n '/data:/ s/ data: // p')
 $TONOS_CLI -u $NETWORK call $GOSH_ADDR setBlob "{\"code\":\"$BLOB_CODE\",\"data\":\"$BLOB_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
+
+echo load `snapshot`-contract
+SNAPSHOT_CODE=$($TVM_LINKER decode --tvc ../blob.tvc | sed -n '/code:/ s/ code: // p')
+SNAPSHOT_DATA=$($TVM_LINKER decode --tvc ../blob.tvc | sed -n '/data:/ s/ data: // p')
+$TONOS_CLI -u $NETWORK call $GOSH_ADDR setBlob "{\"code\":\"$SNAPSHOT_CODE\",\"data\":\"$SNAPSHOT_DATA\"}" --abi $fn_abi --sign $fn_keys > /dev/null || exit 1
 
 echo ===================== GOSH =====================
 echo addr: $GOSH_ADDR

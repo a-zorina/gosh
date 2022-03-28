181608f8455cb293044349edf84f163cb169d110
Gorelyshev Sergey <49919055+SeHor05@users.noreply.github.com>
2022-03-28 16:19:35 +0200

    Update gosh.sol

--- solidity/gosh.sol
+++ solidity/gosh.sol
@@ -16,18 +16,18 @@ contract Gosh is Upgradable{
     TvmCell m_BlobData;
     TvmCell m_codeSnapshot;
     TvmCell m_dataSnapshot;
-    
+
     modifier onlyOwner {
         require(msg.pubkey() == tvm.pubkey(),500);
         _;
     }
-    
+
     constructor() public {
         tvm.accept();
     }
-    
+
     function deployRepository(string name) view public {
-        require(msg.value > 2.2 ton, 100);
+        require(msg.value > 2.6 ton, 100);
         tvm.accept();
         TvmBuilder b;
         b.store(address(this));
@@ -37,29 +37,30 @@ contract Gosh is Upgradable{
         TvmCell _contractflex = tvm.buildStateInit(deployCode, m_RepositoryData);
         TvmCell s1 = tvm.insertPubkey(_contractflex, msg.pubkey());
         address addr = address.makeAddrStd(0, tvm.hash(s1));
-        new Repository {stateInit:s1, value: 1.5 ton, wid: 0} (msg.pubkey(), name);
+        new Repository {stateInit:s1, value: 0.4 ton, wid: 0} (msg.pubkey(), name);
         Repository(addr).setCommit{value: 0.2 ton}(m_CommitCode, m_CommitData);
-        Repository(addr).setBlob{value: 0.2 ton}(m_BlobCode, m_BlobData);    
+        Repository(addr).setBlob{value: 0.2 ton}(m_BlobCode, m_BlobData);
         Repository(addr).setSnapshot{value: 0.2 ton}(m_codeSnapshot, m_dataSnapshot);
+        Repository(addr).deployNewSnapshot{value:1.5 ton}("master");
     }
-    
-    function onCodeUpgrade() internal override {   
+
+    function onCodeUpgrade() internal override {
     }
-    
+
     //Setters
-    
+
     function setRepository(TvmCell code, TvmCell data) public  onlyOwner {
         tvm.accept();
         m_RepositoryCode = code;
         m_RepositoryData = data;
     }
-    
+
     function setCommit(TvmCell code, TvmCell data) public  onlyOwner {
         tvm.accept();
         m_CommitCode = code;
         m_CommitData = data;
     }
-    
+
     function setBlob(TvmCell code, TvmCell data) public  onlyOwner {
         tvm.accept();
         m_BlobCode = code;
@@ -71,9 +72,9 @@ contract Gosh is Upgradable{
         m_codeSnapshot = code;
         m_dataSnapshot = data;
     }
-    
+
     //Getters
-    
+
     function getAddrRepository(string name) external view returns(address) {
         TvmBuilder b;
         b.store(address(this));
@@ -85,15 +86,15 @@ contract Gosh is Upgradable{
         address addr = address.makeAddrStd(0, tvm.hash(s1));
         return addr;
     }
-    
+
     function getRepositoryCode() external view returns(TvmCell) {
         return m_RepositoryCode;
     }
-    
+
     function getCommitCode() external view returns(TvmCell) {
         return m_CommitCode;
     }
-    
+
     function getBlobCode() external view returns(TvmCell) {
         return m_BlobCode;
     }
